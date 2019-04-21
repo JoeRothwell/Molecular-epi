ac945a91b1b66665c9866aeedc333ffb3334910e
JoeRothwell <joerothwell1@gmail.com>
2019-04-21 12:18:21 +0200

    Added metadata subset

--- Lifepath_exploratory.R
+++ Lifepath_exploratory.R
@@ -3,37 +3,49 @@
 library(tidyverse)
 library(readxl)
 
-# Path to food intake data
-path <- "Y:/RepPerso/Fabienne WILM/02_Demandes_ponctuelles/10_LIFEPATH/TABLES"
-list.files(path)
+# Final data files seem to be the following:
 
-# Food intake and other data
-meta1 <- read_csv("D01_20171031_LIFEPATH.csv")
-meta2 <- read_csv("D01_20161018_LIFEPATH.csv")
-meta3 <- read_csv("D01_20150917_LIFEPATH.csv")
+# 1623 observations of 44 intensity variables. Looks scaled version of dat 4 and final prepared data
+ints <- read_tsv("C:/J_ROTHWELL/1507_XMetabolite_std_cpmg_E3N.txt")
+ints <- read_tsv("1507_XMetabolite_std_cpmg_E3N.txt")
+
+# metadata (from XL or csv)
+meta <- read_xlsx("C:/J_ROTHWELL/1603_MatriceY_CohorteE3N_Appar.xlsx", sheet = 6)
+meta <- read.csv("Lifepath_meta.csv")
+
+# subset for baseline characteristics table
+meta0 <- meta %>% 
+  select(CT, AGE, BMI, HANCHE, MENOPAUSE, SMK, DIABETE, Life_Alcohol_Pattern_1, BP, Trait_Horm, 
+         CO, CENTTIMECat1, FASTING, STOCKTIME, BEHAVIOUR, SUBTYPE, HR, Estro_THM, Pg_seul, 
+         SBR, GRADE, STADE, DIAGSAMPLING) %>% 
+  mutate_at(vars(-AGE, -BMI, -HANCHE, -DIAGSAMPLING, -STOCKTIME), as.factor)
+
+# ---- Other files
 
 # Rawest feature data
 raw <- read_delim("C:/J_ROTHWELL/X_AlignedCohorteE3NData_cpmg_ssCitPEG_0612.txt", delim = ";")
 
-# Open other data files
+# Excel files with intermediate steps
 # List of 54 compounds and IDs
-dat1 <- read_xlsx("C:/J_ROTHWELL/1505_E3N_Identification.xlsx")
+xl1 <- read_xlsx("C:/J_ROTHWELL/1505_E3N_Identification.xlsx")
+
+# Previous version of intensities above without scaling
+dat2 <- read_tsv("C:/J_ROTHWELL/1507_XMetaboliteE3N_cpmg.txt")
 
 # List of metabolites and corresponding clusters
-dat2 <- read_xlsx("C:/J_ROTHWELL/1507_ClusterAnnotation_E3N.xlsx")
-
-# 1623 observations of 44 intensity variables. Looks scaled version of dat 4 and final prepared data
-dat3 <- read_tsv("C:/J_ROTHWELL/1507_XMetabolite_std_cpmg_E3N.txt")
-
-# Previous version of file above without scaling
-dat4 <- read_tsv("C:/J_ROTHWELL/1507_XMetaboliteE3N_cpmg.txt")
-
-# metadata
-dat5 <- read_xlsx("C:/J_ROTHWELL/1603_MatriceY_CohorteE3N_Appar.xlsx", sheet = 6)
-
+xl2 <- read_xlsx("C:/J_ROTHWELL/1507_ClusterAnnotation_E3N.xlsx")
 
 # Metadata and intensities
-meta <- read_xlsx("C:/J_ROTHWELL/1603_MatriceY_CohorteE3N_Appar.xlsx", sheet = 6)
+xl4 <- read_xlsx("C:/J_ROTHWELL/1603_MatriceY_CohorteE3N_Appar.xlsx", sheet = 6)
+
+# Subset variables in Table 1 manuscript
+meta <- meta %>% 
+  select(AGE, BMI, HANCHE, MENOPAUSE, SMK, DIABETE, Life_Alcohol_Pattern_1, BP, Trait_Horm, 
+         CO, CENTTIMECat2, FASTING, 
+         STOCKTIME, BEHAVIOUR, SUBTYPE, HR, Estro_THM, Pg_seul, SBR, GRADE, STADE, 
+         DIAGSAMPLINGCat1)
+
+
 ints <- read_tsv("C:/J_ROTHWELL/1507_XMetabolite_std_cpmg_E3N.txt")
 ints1 <- read_tsv("C:/J_ROTHWELL/1507_XMetaboliteE3N_cpmg.txt", skip = 1)
 
@@ -45,6 +57,15 @@ plot(pca1)
 library(pca3d)
 pca2d(pca)
 pca2d(pca1)
+
+# Food intake data ---- 
+path <- "Y:/RepPerso/Fabienne WILM/02_Demandes_ponctuelles/10_LIFEPATH/TABLES"
+list.files(path)
+
+# Food intake and other data
+meta1 <- read_csv("D01_20171031_LIFEPATH.csv")
+meta2 <- read_csv("D01_20161018_LIFEPATH.csv")
+meta3 <- read_csv("D01_20150917_LIFEPATH.csv")
 
 
 
