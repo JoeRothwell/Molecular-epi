5a4c5d867b5636749b408a6f33c5ac6f3de8fdc0
JoeRothwell <joerothwell1@gmail.com>
2019-06-05 17:55:19 +0200

    Added further models

--- Lifepath_exploratory.R
+++ Lifepath_exploratory.R
@@ -81,19 +81,20 @@ meta1 <- meta %>%
 
 # Replace 9999 with NA (for just numeric or all columns)
 meta2 <- meta1 %>% mutate_if(is.numeric, list( ~ na_if(., 9999))) %>% mutate(BP = na_if(BP, 9999))
-meta2.pm <- meta2 %>% filter(MENOPAUSE == 1)
-#meta3 <- meta1 %>% mutate_all(list( ~ na_if(., 9999)))
+
+# Subset pre or post menopausal
+meta2.pre  <- meta2 %>% filter(MENOPAUSE == 0)
+meta2.post <- meta2 %>% filter(MENOPAUSE == 1)
 
 # Conditional logistic regression to get odds ratios for lifestyle factors
 # Same co-variates as in original manuscript
 library(survival)
 fit <- clogit(CT ~ scale(BMI) + SMK + DIABETE + #BP + 
                 scale(RTH) + scale(ALCOHOL) + scale(DURTHSDIAG) + 
-                scale(CENTTIME) + STOCKTIME + strata(MATCH), data = meta2.pm) 
+                scale(CENTTIME) + STOCKTIME + strata(MATCH), data = meta2.pre) 
 # output <- cbind(exp(coef(fit)), exp(confint(fit)))
-library(broom)
-t1 <- tidy(fit) %>% select(-(std.error:p.value))
 
+library(broom)
 t1 <- tidy(fit) %>% select(-(std.error:p.value))
 
 library(metafor)
@@ -109,9 +110,12 @@ text(hh[1], nrow(t1) + 2, "Variable", pos = 4)
 text(hh[2], nrow(t1) + 2, "OR [95% CI]", pos = 2)
 # matching factors removed!
 
-# CLR models to get odds ratios for metabolites
-data <- left_join(meta2, ints, by = "CODBMB")
+# Metabolite risk models --------------------------------------------------
 
+# CLR models to get odds ratios for metabolites
+
+data <- left_join(meta2, ints, by = "CODBMB")
+  
 clr <- function(x) { 
   clogit(CT ~ x + scale(BMI) + SMK + DIABETE + #BP + 
         scale(RTH) + scale(ALCOHOL) + scale(DURTHSDIAG) + 
@@ -124,6 +128,7 @@ t2 <- map_df(multifit, tidy) %>% filter(term == "x")
 
 dev.off()
 par(mar=c(5,4,1,2))
+library(metafor)
 forest(t2$estimate, ci.lb = t2$conf.low, ci.ub = t2$conf.high, refline = 1, #xlab = xtitle, 
        xlab = "Multivariable adjusted odds ratio",
        transf = exp, pch = 18, psize = 1, slab = names(multifit))
@@ -132,9 +137,15 @@ text(hh[1], nrow(t2) + 2, "Compound", pos = 4)
 text(hh[2], nrow(t2) + 2, "OR [95% CI]", pos = 2)
        #alim = c(0,2), xlim = c(-1, 3)) 
 
+
 # Funnel plots for metabolites
 funnel(x = t2$estimate, sei = t2$std.error)
 funnel(x = t2$estimate, sei = t2$std.error, yaxis = "vi")
+
+# Investigation of Ethanol
+boxplot(data$Ethanol ~ data$CT + data$MENOPAUSE, varwidth = T, outline = F,
+        names = c("Control, pre", "Case, pre", "Control, post", "Case, post"),
+        col = "dodgerblue", ylab = "Plasma ethanol conc (scaled)")
 
 # Non-metabolite model lme4
 library(lme4)
