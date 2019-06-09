e87ed67c6fec5d1f21cb03fc76daa8180d483f05
JoeRothwell <johndoe@example.com>
2019-06-09 18:04:21 +0200

    Added mixed-effect models and PCPR2

--- BC_multivariate_models.R
+++ BC_multivariate_models.R
@@ -4,12 +4,29 @@ library(tidyverse)
 # Read 1623 observations of 44 intensity variables (appears to be final scaled data) and metadata
 ints <- read_tsv("1507_XMetabolite_std_cpmg_E3N.txt")
 meta <- read.csv("Lifepath_meta.csv")
-alldata <- inner_join(meta, ints, by = "CODBMB")
-ints1 <- alldata %>% select(`3Hydroxybutyrate`:Succinate)
+dat <- inner_join(meta, ints, by = "CODBMB")
+ints1 <- dat %>% select(`3Hydroxybutyrate`:Succinate) %>% as.matrix
 
-# Adjust using residuals method
-adj <- function(x) residuals(lm(x ~ BMI + SMK + DIABETE, data = alldata))
-adjmat <- apply(ints1, 2, adj)
+# Adjust using residuals method (mixed effects works better)
+library(lme4)
+adj <- function(x) residuals(lm(x ~ PLACE + AGE + BMI + DIABETE + CENTTIMECat1, data = dat))
+adj1 <- function(x) residuals(lmer(x ~ AGE + BMI + DIABETE + SAMPYEAR + (1|PLACE), data = dat)) 
+
+adjmat <- apply(ints1, 2, adj1)
+
+Z_Meta <- dat %>%
+  select(WEEKS, PLACE, AGE, BMI, MENOPAUSE, FASTING, SMK, DIABETE, CENTTIMECat1, SAMPYEAR, STOCKTIME) %>%
+  mutate_at(vars(-AGE, -BMI, -WEEKS, -STOCKTIME), as.factor)
+
+# Check with PCPR2 before and after adjustment
+library(pcpr2)
+props.raw <- runPCPR2(ints1, Z_Meta)
+props.adj <- runPCPR2(adjmat, Z_Meta)
+par(mfrow = c(2,1))
+plotProp(props.raw, main = "Sources of variation in 1582 cases and controls")
+plotProp(props.raw, main = "No adjustment to metabolite concentrations", font.main = 1)
+plotProp(props.adj, main = "Residual-adjusted metabolite matrix", font.main = 1)
+
 
 # OPLS-DA on residual-adjusted concentrations
 library(mixOmics)
