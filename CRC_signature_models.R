015a1e4e356ff4ff4598aadfeaad9b3622ed7410
JoeRothwell <joerothwell1@gmail.com>
2019-08-27 17:32:00 +0200

    Minor updates

--- CRC_signature_models.R
+++ CRC_signature_models.R
@@ -173,8 +173,8 @@ ll2 <- list(fit3, fit5, fit7)
 library(broom)
 library(tidyverse)
 t2 <- map_df(ll2, tidy) %>% filter(term == "score.2.comps")
-studies <- data.frame(CC = c("Large", rep("Small", 2)), nvec = map_int(ll2, 10),
-  metabolites = c(rep("Biocrates", 2), "Fatty acids"))
+studies <- data.frame(CC = c("Case-control B", rep("Case-control A", 2)), nvec = map_int(ll2, 10),
+  metabolites = c(rep("Endogenous", 2), "Fatty acids"))
 
 par(mar=c(5,4,1,2))
 library(metafor)
