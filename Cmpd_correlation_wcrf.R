b4b27b4a1c95d68acc8b4fb313045eb7d2ece87f
JoeRothwell <joerothwell1@gmail.com>
2019-10-18 11:22:54 +0200

    Added coloured labels

--- Cmpd_correlation_wcrf.R
+++ Cmpd_correlation_wcrf.R
@@ -1,12 +1,13 @@
 # Correlations between Biocrates compounds and FAs in small CC
 source("CRC_data_prep.R")
-source("Metabolic_signatures.R")
 
+# Get compounds and idepics from CRC1
 FAs.ID <- get.plsdata(CRCfa1, cor.data = T)
 Bioc.ID <- select.ctrl.cmpds(crc1, cor.data = T)
 
 dat <- inner_join(FAs.ID, Bioc.ID, by = "Idepic") %>% select(-Idepic) 
 
+# Get compound metadata for short names
 meta.bioc <- read.csv("Biocrates_cmpd_metadata.csv") %>% select(Compound, displayname)
 meta.fa   <- read.csv("FA_compound_data.csv") %>% select(Compound, displayname)
 meta <- bind_rows(meta.bioc, meta.fa)
@@ -15,19 +16,25 @@ mm <- data.frame(Compound = colnames(dat))
 
 all <- left_join(mm, meta, by = "Compound")
 
+# Calculate correlations
 cormat <- cor(dat, use = "pairwise.complete.obs")
 rownames(cormat) <- all$displayname
 
+# Correlation matrix (large)
 library(corrplot)
 corrplot(cormat, tl.col = "black", tl.cex = 0.5, order = "hclust", method = "square")
 
-#clustmat <- hclust(dist(cormat))
-#dend1 <- as.dendrogram(clustmat)
+# Dendrogram. First get order of clustered compounds
+dend <- cormat %>% dist %>% hclust %>% as.dendrogram
+
+# Make ordered numeric vector for label colour
+ord <- order.dendrogram(dend)
+cmpds <- c(rep(2, 34), rep(1, 180-34))[ord]
 
 library(dendextend)
 dend1 <- cormat %>% dist %>% hclust %>% as.dendrogram %>%
   set("branches_k_color", k=4) %>% 
-  #set("labels_col", value = c(1,2,3,4), k=4) %>%
+  set("labels_col", cmpds) %>%
   set("labels_cex", c(0.6)) #%>%
 #hang.dendrogram(hang_height = 0.3)
 
