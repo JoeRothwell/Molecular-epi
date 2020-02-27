b55433c7af03e09311aca2b2dcf4d90a9a59e686
JoeRothwell <joerothwell1@gmail.com>
2020-02-27 23:23:38 +0100

    Added ethanol analysis

--- BC_manuscript_figs.R
+++ BC_manuscript_figs.R
@@ -1,5 +1,6 @@
 library(survival)
 library(broom)
+library(ggrepel)
 source("BC_prep_data.R")
 
 fits0 <- apply(ints, 2, function(x) clogit(CT ~ BMI + SMK + DIABETE + RTH + ALCOHOL + 
@@ -57,3 +58,44 @@ p3 <- ggplot(t1, aes(exp(estimate), log10(p.value))) + geom_point() + theme_bw()
 
 library(cowplot)
 plot_grid(p1, p3, p2, labels = c('A', 'B', 'C'), label_size = 12)
+
+# Sensitivity analysis adjusted ethanol
+# All subjects
+meta.e <- cbind(meta, ethanol = ints[, 14])
+
+fits0 <- apply(ints, 2, function(x) clogit(CT ~ BMI + SMK + DIABETE + RTH + ALCOHOL +
+          ethanol +
+          DURTHSBMB + CENTTIME + STOCKTIME + strata(MATCH) + x, data = meta.e))
+
+t1 <- map_df(fits0, tidy) %>% filter(str_detect(term, "x")) %>% bind_cols(cmpd.meta) %>% 
+  arrange(description)
+
+p1 <- ggplot(t1, aes(exp(estimate), log10(p.value))) + geom_point() + theme_bw() +
+  xlim(0.8, 1.2) +
+  scale_y_reverse(breaks = c(-2, -1, 0), labels = function(x) 10^x) +
+  xlab("Odds ratio per SD increase concentration") + ylab("P-value") +
+  geom_text_repel(aes(label = display_name), size = 3,
+                  data = t1[t1$p.value < 0.3, ]) +
+  geom_hline(yintercept = log10(0.05), linetype = "dashed") +
+  geom_vline(xintercept = 1, linetype = "dashed") +
+  ggtitle("All subjects")
+
+# Pre-menopausal
+meta1e <- meta.e[pre, ]
+# Note: need to remove hormone treatment therapy variable DURTHSBMB
+fits1 <- apply(ints[pre, ], 2, function(x) clogit(CT ~ BMI + SMK + DIABETE + RTH + ALCOHOL +
+                                                    ethanol +
+  CENTTIME + STOCKTIME + strata(MATCH) + x, data = meta1e[pre, ]))
+
+t1 <- map_df(fits1, tidy) %>% filter(str_detect(term, "x")) %>% bind_cols(cmpd.meta) %>% 
+  arrange(description)
+
+p2 <- ggplot(t1, aes(exp(estimate), log10(p.value))) + geom_point() + theme_bw() +
+  xlim(0.3, 1.7) + 
+  scale_y_reverse(breaks = c(-3, -2, -1, 0), labels = function(x) 10^x) +
+  xlab("Odds ratio per SD increase concentration") + ylab("P-value") +
+  geom_text_repel(aes(label = display_name), size = 3,
+                  data = t1[t1$p.value < 0.04, ] ) +
+  geom_hline(yintercept = c(log10(0.05), log10(0.014)), linetype = "dashed") +
+  geom_vline(xintercept = 1, linetype = "dashed") +
+  ggtitle("Pre-menopausal")
