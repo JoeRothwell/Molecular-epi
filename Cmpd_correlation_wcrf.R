cc72213d39bf23e274fd81efbac2d87f14082a9c
JoeRothwell <joerothwell1@gmail.com>
2020-03-07 09:09:03 +0100

    Improved heatmap and reordered script

--- Cmpd_correlation_wcrf.R
+++ Cmpd_correlation_wcrf.R
@@ -3,10 +3,11 @@ source("CRC_prep_data.R")
 source("CRC_get_signatures.R")
 
 # Get compounds and idepics from CRC1 and join together by Idepic
-FAs.ID <- get.plsdata(CRCfa1, cor.data = T)
-Bioc.ID <- select.ctrl.cmpds(crc1, cor.data = T)
+FAs.ID <- CRCfa1 %>% select(Idepic, one_of(common.cols))
+Bioc.ID <- select.ctrl.cmpds(crc1, cor.data = T) %>% distinct() # remove dupes
 
 dat <- inner_join(FAs.ID, Bioc.ID, by = "Idepic") %>% select(-Idepic) 
+# 877 FA subjects, 937 Biocrates, 876 in overlap
 
 # Get compound metadata for short names
 meta.bioc <- read_csv("Biocrates_cmpd_metadata.csv") %>% select(Compound, displayname)
@@ -22,27 +23,6 @@ cormat <- cor(dat, use = "pairwise.complete.obs")
 rownames(cormat) <- all$displayname
 colnames(cormat) <- all$displayname
 
-# Correlation matrix (large)
-library(corrplot)
-corrplot(cormat, tl.col = "black", tl.cex = 0.5, order = "hclust", method = "square")
-
-# Dendrogram. First get order of clustered compounds
-dend <- cormat %>% dist %>% hclust %>% as.dendrogram
-
-# Make ordered numeric vector for label colour
-ord <- order.dendrogram(dend)
-cmpds <- c(rep(2, 34), rep(1, 180-34))[ord]
-
-library(dendextend)
-dend1 <- cormat %>% dist %>% hclust %>% as.dendrogram %>%
-  set("branches_k_color", k=4) %>% 
-  set("labels_col", cmpds) %>%
-  set("labels_cex", c(0.6)) #%>%
-#hang.dendrogram(hang_height = 0.3)
-
-library(circlize)
-circlize_dendrogram(dend1, dend_track_height = 0.85)
-
 # Get table of correlations in descending order
 library(reshape2)
 all.correlations <- melt(cormat) %>% filter(value != 1) %>% arrange(desc(value))
@@ -53,7 +33,6 @@ t1 <- all.correlations %>% filter(Var1 %in% meta.fa$displayname & Var2 %in% meta
 t2 <- melt(cormat) %>% 
   filter(value > 0.55, Var1 %in% meta.fa$displayname & Var2 %in% meta.bioc$displayname)
 colnames(t2) <- c("displayname2", "displayname", "value")
-
 
 # Heatmap Biocrates vs fatty acids only
 mat <- acast(t1, Var2 ~ Var1, value.var = "value")
@@ -76,7 +55,13 @@ library(ComplexHeatmap)
 library(circlize)
 library(RColorBrewer)
 
-row_ha <- rowAnnotation(Class = annotation_df$class)
+# Make object and legend for class annotations
+row_ha <- rowAnnotation(Class = annotation_df$class, annotation_legend_param = list(
+                        labels = c("Acylcarnitines", "Amino acids", "Biogenic amines", 
+                                   "LysoPC", "Monosacchaaride", "PC (acyl-acyl)", 
+                                   "PC (acyl-alkyl)", "Sphingolipids")))
+
+# Make object for annotated compounds on right
 ha <- rowAnnotation(foo = anno_mark(at = cmpd.pos, labels = corr.cmpds, 
                                     labels_gp = gpar(fontsize = 9)))
 
@@ -85,7 +70,7 @@ rownames(mat) <- NULL
 Heatmap(mat, col = colorRampPalette(rev(brewer.pal(n = 7, name ="RdYlBu")))(100),
         #column_title = "Fatty acids", row_title = "Endogenous metabolites", 
         cluster_rows = T,
-        #column_km = 3, row_km = 3, 
+        #cluster_columns = T,
         #row_split = annotation_df$class,
         row_title = NULL,
         #row_names_side = "right", show_row_names = T,
@@ -94,8 +79,32 @@ Heatmap(mat, col = colorRampPalette(rev(brewer.pal(n = 7, name ="RdYlBu")))(100)
         left_annotation = row_ha,
         right_annotation = ha,
         heatmap_legend_param = list(title = "Correlation"),
-        #row_gap = unit(0, "mm"),
+        row_gap = unit(0, "mm"),
         border = F)
+
+
+# Correlation matrix (large) (supplemental data)
+library(corrplot)
+corrplot(cormat, tl.col = "black", tl.cex = 0.5, order = "hclust", method = "square")
+
+# Not in manuscript
+
+# Dendrogram. First get order of clustered compounds
+dend <- cormat %>% dist %>% hclust %>% as.dendrogram
+
+# Make ordered numeric vector for label colour
+ord <- order.dendrogram(dend)
+cmpds <- c(rep(2, 34), rep(1, 180-34))[ord]
+
+library(dendextend)
+dend1 <- cormat %>% dist %>% hclust %>% as.dendrogram %>%
+  set("branches_k_color", k=4) %>% 
+  set("labels_col", cmpds) %>%
+  set("labels_cex", c(0.6)) #%>%
+#hang.dendrogram(hang_height = 0.3)
+
+library(circlize)
+circlize_dendrogram(dend1, dend_track_height = 0.85)
 
 
 
