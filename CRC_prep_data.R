d31ffd9b996532cbd8369fc142a217e1670b3b96
JoeRothwell <joerothwell1@gmail.com>
2020-06-01 22:31:29 +0200

    Tidied code

--- CRC_prep_data.R
+++ CRC_prep_data.R
@@ -12,9 +12,9 @@ library(haven)
 var.list <- c("Country", "Center", "Sex", "Match_Caseset", "L_School", #"Smoke_Int", 
               "Smoke_Stat", "Smoke_Intensity", "Fasting_C", "Menopause", "Phase_Mnscycle")
 
+# set D_Dgclrt of controls to that of corresponding cases, calculate followup time and 
+# and get cancer site variables
 meta <- read_dta("clrt_caco.dta") %>% 
-  # set D_Dgclrt of controls to that of corresponding cases, calculate followup time and 
-  # and get cancer site variables
   mutate(Tfollowup.days = D_Dgclrt - D_Bld_Coll, Tfollowup = Tfollowup.days/365.25, 
          location = case_when(
     Case_Mal_Colon_Prox == 1 ~ 1, Case_Mal_Colon_Dist == 1 ~ 2,
@@ -40,9 +40,12 @@ crc1 <- read_sas("clrt_caco_metabo.sas7bdat") %>% filter(!is.na(Aminoacid_Glu)) 
 colon1 <- crc1 %>% group_by(Match_Caseset) %>% 
   filter(max(location, na.rm = T) == 1 | max(location, na.rm = T) == 2) %>% ungroup(Match_Caseset)
 
+# Get cases diagnosed after 2 years only
+crc1t <- crc1 %>% group_by(Match_Caseset) %>% filter(max(Tfollowup, na.rm = T) > 2) %>% ungroup()
+
 # Subset male or female
-#crc1.ma <- crc1 %>% filter(Sex == 1)
-#crc1.fe <- crc1 %>% filter(Sex == 2)
+crc1m <- crc1 %>% filter(Sex == 1)
+crc1f <- crc1 %>% filter(Sex == 2)
 
 
 # Large case-control subset (from Jelena)------------
@@ -62,9 +65,12 @@ colon2 <- crc2 %>% group_by(Match_Caseset) %>%
 rectal2 <- crc2 %>% group_by(Match_Caseset) %>% 
   filter(max(location, na.rm = T) == 3) %>% ungroup(Match_Caseset)
 
+# Get cases diagnosed after 2 years only
+crc2t <- crc2 %>% group_by(Match_Caseset) %>% filter(max(Tfollowup, na.rm = T) > 2) %>% ungroup()
+
 # Subset male or female
-#crc2.ma <- crc1 %>% filter(Sex == 1)
-#crc2.fe <- crc1 %>% filter(Sex == 2)
+crc2m <- crc2 %>% filter(Sex == 1)
+crc2f <- crc2 %>% filter(Sex == 2)
 
 
 # EPIC pooled controls--------
@@ -79,7 +85,6 @@ ctrl <- read_dta("obes_metabo.dta") %>% mutate(Study =
   filter(!(Study %in% c("Colonrectum_L", "Colonrectum_S")), Fasting_C == 2) %>%
   filter(Country != 6)
 
-#print(paste(nrow(ctrl), "fasted controls read"))
 # 1799 fasted subjects left, 1741 after removal of Greece
 
 # Get controls dataset for crc1 and crc2 compounds only, and put in same order
@@ -101,13 +106,18 @@ ctrls0 <- ctrls[, intersect(colnames(ctrlA), colnames(ctrlB))]
 # Gets common compounds between CC and EPIC controls and puts them in the same order.
 
 # Get CRC dataset from Elom and join WCRF scores. Convert categorical co-variates to factors
-crc1fa <- read_dta("Database_Fatty acids.dta") %>% 
+crc3 <- read_dta("Database_Fatty acids.dta") %>% 
   left_join(meta, by = "Idepic", suffix = c("", "_1")) %>%
   mutate_at(vars(var.list), as.factor) %>%
   mutate(Smoke_Int = fct_collapse(Smoke_Intensity, Other = c("8", "9", "10"))) %>%
   filter(Country != 6)
 
-#crc1faf <- crc1fa %>% filter(Sex == 2)
+# Get male and female subsets
+crc3m <- crc3 %>% filter(Sex == 1)
+crc3f <- crc3 %>% filter(Sex == 2)
+
+# Get cases diagnosed after 2 years only
+crc3t <- crc3 %>% group_by(Match_Caseset) %>% filter(max(Tfollowup, na.rm = T) > 2) %>% ungroup()
 
 # Get dataset for PLS modelling (all EPIC controls). Exclude compounds with many missings
 # Note: new version from Carine received 18/11/2018 with technical covariates
@@ -117,23 +127,15 @@ fa.ctrl$N_Serie <- as.numeric(fa.ctrl$N_Serie)
 # Get sex of participants
 epic.vars <- read.csv("full_epic_sex.csv")
 fa.ctrl <- left_join(fa.ctrl, epic.vars, by = "Idepic")
-#fa.ctrl.f <- fa.ctrl %>% filter(Sex == 2)
-# Not used because 118 and 4121 M and F respectively
 
 # Convert variables to factors
 var.list <- c("Country", "Center", "STUDY", "LABO")
 fa.ctrl <- fa.ctrl %>% mutate_at(vars(var.list), as.factor)
 
 # Subset concentrations for CRC and controls
-crcfa <- crc1fa  %>% select(P14_0 : PCLA_9t_11c) 
+crcfa <- crc3  %>% select(P14_0 : PCLA_9t_11c) 
 concs <- fa.ctrl %>% select(P14_0 : PCLA_9t_11c, -P24_0, -P20_0)
-common.cols <- intersect(colnames(concs), colnames(crcfa))
 ctrlC <- fa.ctrl[, intersect(colnames(concs), colnames(crcfa))]
-
-#concs.f <- fa.ctrl.f %>% select(P14_0 : PCLA_9t_11c, -P24_0, -P20_0)
-
-# Subset only controls from FAs and CRC A for correlation (to remove)
-#CRCfa.ctrl <- crc1fa %>% filter(Cncr_Caco_Clrt == 0) %>% select(Idepic, P14_0 : PCLA_9t_11c) 
 
 # Number of control profiles for biocrates and fatty acids
 nrow(ctrl)
@@ -143,12 +145,3 @@ nrow(fa.ctrl)
 length(intersect(ctrl$Idepic, fa.ctrl$Idepic))
 #intersect(ctrl$Idepic, fa.ctrl$Idepic)
 
-# Old subset for sex-specific signature
-#ctrl.m <- ctrl %>% filter(Sex == 1)
-#ctrl.f <- ctrl %>% filter(Sex == 2)
-
-# Data for sex-specific signatures
-#ctrlAm <- select.ctrl.cmpds(list(ctrl.m, crc1.ma))
-#ctrlAf <- select.ctrl.cmpds(list(ctrl.f, crc1.fe))
-#ctrlBm <- select.ctrl.cmpds(list(ctrl.m, crc2.ma))
-#ctrlBf <- select.ctrl.cmpds(list(ctrl.f, crc2.fe))
