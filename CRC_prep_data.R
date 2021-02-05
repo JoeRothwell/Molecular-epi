3c287c1ccb06232d17852a287b1892a9bbda9445
Joe <joerothwell1@gmail.com>
2021-02-05 16:08:09 +0100

    Added data prep for amino acids

--- CRC_prep_data.R
+++ CRC_prep_data.R
@@ -7,7 +7,8 @@ library(lubridate)
 # WARNING: do not use case-control status from this dataset. Use metabolomics datasets only.
 # Remove duplicated Idepics (with dplyr or base). Also get follow up time and colorectal site
 var.list <- c("Country", "Center", "Sex", "Match_Caseset", "L_School", #"Smoke_Int", 
-              "Smoke_Stat", "Smoke_Intensity", "Fasting_C", "Menopause", "Phase_Mnscycle")
+              "Smoke_Stat", "Smoke_Intensity", "Fasting_C", "Menopause", "Phase_Mnscycle",
+              "Alc_Drinker", "Pa_Total")
 
 # set D_Dgclrt of controls to that of corresponding cases, calculate followup time and 
 # and get colorectal subsite variables
@@ -16,19 +17,26 @@ meta <- read_dta("clrt_caco.dta") %>%
          location = case_when(
            Case_Mal_Colon_Prox == 1 ~ 1, Case_Mal_Colon_Dist == 1 ~ 2,
            Case_Mal_Colon_Nos  == 1 ~ 4, Case_Mal_Rectum     == 1 ~ 3)) %>%
-  group_by(Match_Caseset) %>% fill(c(D_Dgclrt, location), .direction = "downup") %>% ungroup() %>%
-  select(-Match_Caseset, -Cncr_Caco_Clrt) %>%
+  group_by(Match_Caseset) %>% fill(c(D_Dgclrt, location), .direction = "downup") %>% 
+  ungroup() %>% select(-Match_Caseset, -Cncr_Caco_Clrt) %>%
   distinct(Idepic, .keep_all = T)
 
-# Small case-control subset. Use glutamate to correctly subset biocrates data, join metadata. 
-crc1 <- read_sas("clrt_caco_metabo.sas7bdat") %>% filter(!is.na(Aminoacid_Glu)) %>%
+# Small case-control subset (p180)
+# Use Batch_MetBio to correctly subset biocrates data, join metadata. 
+crc1 <- read_sas("clrt_caco_metabo.sas7bdat") %>% 
+  filter(!is.na(Batch_MetBio)) %>%
   left_join(meta, by = "Idepic", suffix = c("_1", "")) %>%
   mutate_at(vars(var.list), as.factor) %>% 
   mutate(Smoke_Int = fct_collapse(Smoke_Intensity, Other = c("8", "9", "10"))) %>% 
   filter(Country != 6) # Greece removed
 
+# Add categorical BMI
+crc1$Bmi_Cat<- as.factor(cut(crc1$Bmi_C, c(0,25,30,99), labels=FALSE, right=FALSE))
+
 # Get colon cancer only (ungroup to stop Match_Caseset from being readded later)
-colon1 <- crc1 %>% filter(location == 1 | location  == 2)
+# Amino acids study
+colon1 <- crc1 %>% filter(!location %in% 3 & Fasting_C == 2) %>% 
+  group_by(Match_Caseset) %>% filter(n() == 2) %>% ungroup()
 
 # Subsites
 rectal1 <- crc1 %>% group_by(Match_Caseset) %>% filter(max(location, na.rm = T) == 3) %>% ungroup(Match_Caseset)
@@ -40,17 +48,20 @@ crc1m <- crc1 %>% filter(Sex == 1)
 crc1f <- crc1 %>% filter(Sex == 2)
 crc1t <- crc1 %>% group_by(Match_Caseset) %>% filter(max(Tfollowup, na.rm = T) > 2) %>% ungroup()
 
-# Large case-control subset (from Jelena)
+# Large case-control subset (p150 from Jelena)
 crc2 <- read_csv("biocrates_p150.csv") %>% 
   select(Match_Caseset, Cncr_Caco_Clrt, 
          ends_with("Idepic"), matches("(carn|oacid|genic|roph|ingo|Sugars)[_]"), -contains("tdq")) %>%
-  inner_join(meta, by = "Idepic") %>% mutate_at(vars(var.list), as.factor) %>% 
+  inner_join(meta, by = "Idepic") %>% 
+  mutate_at(vars(var.list), as.factor) %>% 
   mutate(Smoke_Int = fct_collapse(Smoke_Intensity, Other = c("8", "9", "10"))) %>%
   filter(Country != 6)
 
-# Get colon cancer or rectal cancer subsets
-colon2 <- crc2 %>% group_by(Match_Caseset) %>% 
-  filter(max(location, na.rm = T) == 1 | max(location, na.rm = T) == 2) %>% ungroup(Match_Caseset)
+crc2$Bmi_Cat<- as.factor(cut(crc2$Bmi_C, c(0,25,30,99), labels=FALSE, right=FALSE))
+
+# Get colon cancer for amino acids study
+colon2 <- crc2 %>% filter(!location %in% 3 & Fasting_C == 2) %>% 
+  group_by(Match_Caseset) %>% filter(n() == 2) %>% ungroup()
 
 rectal2 <- crc2 %>% group_by(Match_Caseset) %>% filter(max(location, na.rm = T) == 3) %>% ungroup(Match_Caseset)
 prox2 <- crc2 %>% group_by(Match_Caseset) %>% filter(max(location, na.rm = T) == 1) %>% ungroup(Match_Caseset)
@@ -64,7 +75,7 @@ crc2m <- crc2 %>% filter(Sex == 1)
 crc2f <- crc2 %>% filter(Sex == 2)
 
 ### 11 september 2020, reviewers revisions for CGH ###
-# Merge crc1 and crc2 to make complete dataset
+# Merge crc1 and crc2 to make complete dataset (now moved to CRC_prep_data_rev)
 crc <- bind_rows(crc1, crc2, .id = "lab")
 crc$lab <- as.factor(crc$lab)
 
