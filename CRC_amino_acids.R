3a2beee1546777827a5a1fffa8875452b190dd15
Joe <joerothwell1@gmail.com>
2021-02-11 22:37:44 +0100

    Added meta analyses

--- CRC_amino_acids.R
+++ CRC_amino_acids.R
@@ -11,11 +11,12 @@ mat1 <- colon1 %>% select(colnames(mat0)) %>% na_if(0) #738, 698 w/o Greece
 mat2 <- colon2 %>% select(colnames(mat0)) %>% na_if(0) #564, 556 w/o Greece
 
 library(zoo)
-scalemat1 <- mat1 %>% na.aggregate(FUN = function(x) min(x)/2) %>% #log2 %>% 
-  scale
-scalemat2 <- mat2 %>% na.aggregate(FUN = function(x) min(x)/2) %>% #log2 %>% 
-  scale
+scalemat1 <- mat1 %>% na.aggregate(FUN = function(x) min(x)/2) %>% scale
+scalemat2 <- mat2 %>% na.aggregate(FUN = function(x) min(x)/2) %>% scale
 
+# Plot distributions
+plot.ts(mat1[, 1:6], type = "p", main = "Arg to Orn")
+plot.ts(mat1[, 7:13], type = "p", main = "Phe to Val")
 
 ### Continuous models per SD increase
 # Define function to apply across quartiles (already matched by lab)
@@ -53,13 +54,17 @@ paste(round(mods1$estimate, 2), " (", round(mods1$conf.low, 2), "-",
 paste(round(mods2$estimate, 2), " (", round(mods2$conf.low, 2), "-", 
       round(mods2$conf.high, 2), ")", sep = "") %>% as.tibble()
 
+# Meta analysis by nesting
+library(metafor)
+cont <- bind_rows(mods1, mods2) %>% group_by(compound) %>% nest() %>% 
+  mutate(mods = lapply(data, function(df) rma(estimate, sei = std.error, data=df, method="REML")))
+ma.cont <- lapply(cont$mods, "[", c("b", "ci.lb", "ci.ub", "se", "I2", "QEp"))
+results1 <- map_df(ma.cont, bind_rows) %>% mutate_all(~round(., 2))
 
-### Categorical analysis. Replace concentrations with quartile categories
-#mat3 <- mat1 %>% mutate_all(~cut_number(., n = 4))
-#mat3a <- apply(mat1, 2, function(x) cut(x, breaks = quantile(x, include.lowest = T), labels = 1:4))
 
-# Need to alter function to get categories with cutpoints based on controls
-cutct <- function(x) cut(x, breaks = quantile(x[colon1$Cncr_Caco_Clrt == 0]), include.lowest = T, labels = 1:4)
+### Categorical analysis. Need to alter function to get categories with cutpoints based on controls
+ctrl <- colon1$Cncr_Caco_Clrt == 0
+cutct <- function(x) cut(x, breaks = quantile(x[ctrl]), include.lowest = T, labels = 1:4)
 mat3 <- apply(mat1, 2, cutct)
 
 fits1 <- apply(mat3, 2, multiclr, dat = colon1) %>% 
@@ -72,8 +77,10 @@ hrci <- paste(round(fits1$estimate, 2), " (", round(fits1$conf.low, 2), "-",
         round(fits1$conf.high, 2), ")", sep = "") %>% as.tibble()
 
 # Get p-trend by entering quartile cutoffs into the model as continuous variables
-cutpts <- function(x) cut(x, breaks = quantile(x[colon1$Cncr_Caco_Clrt == 0]), include.lowest = T)
-mat3a <- apply(mat1, 2, cutpts)
+library(Hmisc)
+cutpts <- function(x) as.numeric(as.character(cut2(x, cuts = quantile(x[ctrl]), levels.mean = T)))
+mat3b <- map_df(mat1, cutpts)
+trends <- apply(mat3b, 2, multiclr, dat = colon1) %>% map_df( ~ tidy(.)) %>% filter(grepl("x", term))
 
 #mat4 <- mat2 %>% mutate_all(~cut_number(., n = 4, labels = 1:4))
 cutct <- function(x) cut(x, breaks = quantile(x[colon2$Cncr_Caco_Clrt == 0]), include.lowest = T, labels = 1:4)
@@ -89,6 +96,18 @@ hrci <- paste(round(fits1$estimate, 2), " (", round(fits1$conf.low, 2), "-",
         round(fits1$conf.high, 2), ")", sep = "") %>% as.tibble() #%>%
         #mutate(quartile = rep(c("X2", "X3", "X4"), 13)) %>%
         #pivot_wider(names_from = quartile, values_from = value)
+
+ctrl <- colon2$Cncr_Caco_Clrt == 0
+cutpts <- function(x) as.numeric(as.character(cut2(x, cuts = quantile(x[ctrl]), levels.mean = T)))
+mat4b <- map_df(mat2, cutpts)
+trends <- apply(mat4b, 2, multiclr, dat = colon2) %>% map_df( ~ tidy(.)) %>% filter(grepl("x", term))
+
+# Meta-analysis
+cats <- bind_rows(fits1, fits2) %>% group_by(compound, term) %>% nest() %>% 
+  mutate(mods = lapply(data, function(df) rma(estimate, sei = std.error, data=df, method="REML")))
+ma.cat <- lapply(cats$mods, "[", c("b", "ci.lb", "ci.ub", "se", "I2", "QEp"))
+results2 <- map_df(ma.cat, bind_rows) %>% mutate_all(~round(., 2)) %>% 
+  mutate(quartile = cats$term) %>% arrange(quartile)
 
 # Bind continuous and categorical results together
 p180 <- bind_rows(Continuous = mods1, Categorical = fits1, .id = "Analysis")
