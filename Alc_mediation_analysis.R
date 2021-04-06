914d62403407badb1910de0899292263537e135d
Joe <joerothwell1@gmail.com>
2021-04-06 22:09:48 +0200

    Added new mediators and improved code

--- Alc_mediation_analysis.R
+++ Alc_mediation_analysis.R
@@ -13,8 +13,8 @@ crc3.ph$Qe_Alc_cat <- dicho(crc3.ph$Qe_Alc, dich.by = 0.5)
 crc1$Qe_Alc_cat <- dicho(crc1$Qe_Alc, dich.by = 0.5)
 
 # Both case-controls without and with gate variable
-fit1 <- clogit(update(base, ~. + I(Qe_Alc/12)), data = crc.ph) #1.06 (1.00-1.11)
-fit2 <- clogit(update(base, ~. + I(Qe_Alc/12) + Qe_Alc_cat), data = crc.ph) #1.07 (1.01-1.13)
+#fit1 <- clogit(update(base, ~. + I(Qe_Alc/12)), data = crc.ph) #1.06 (1.00-1.11)
+#fit2 <- clogit(update(base, ~. + I(Qe_Alc/12) + Qe_Alc_cat), data = crc.ph) #1.07 (1.01-1.13)
 
 # Small case-control without and with gate variable
 fit3 <- clogit(update(base, ~. + I(Qe_Alc/12)), data = crc1) #1.14 (1.00-1.28)
@@ -24,12 +24,12 @@ fit4 <- clogit(update(base, ~. + I(Qe_Alc/12) + Qe_Alc_cat), data = crc1) #1.18 
 fit5 <- clogit(update(base, ~. + I(Qe_Alc/12)), data = crc3.ph) #1.15 (1.00-1.29)
 fit6 <- clogit(update(base, ~. + I(Qe_Alc/12) + Qe_Alc_cat), data = crc3.ph) #1.19 (1.04-1.35)
 
-modlist <- list(fit1, fit2, fit3, fit4, fit5, fit6)
+#modlist <- list(fit1, fit2, fit3, fit4, fit5, fit6)
 
 # Summarise OR (CI)
 library(broom)
-scomods <- map_df(modlist, ~tidy(., exponentiate = T)) %>% filter(str_detect(term, "Alc/12")) %>%
-  mutate_if(is.numeric, ~round(., 2)) %>% unite(OR.CI, estimate, conf.low, conf.high, sep = "-")
+#scomods <- map_df(modlist, ~tidy(., exponentiate = T)) %>% filter(str_detect(term, "Alc/12")) %>%
+#  mutate_if(is.numeric, ~round(., 2)) %>% unite(OR.CI, estimate, conf.low, conf.high, sep = "-")
 
 # Scaled and categorical variables (no associations)
 #fit2 <- clogit(update(base, ~. + lab + scale(Qe_Alc)), data = crc.ph)
@@ -38,124 +38,168 @@ scomods <- map_df(modlist, ~tidy(., exponentiate = T)) %>% filter(str_detect(ter
 ### Calculation of NDE, NIE and RD ratio from Van Steenland 2010 (pg 1342)
 # Run Make_alc_mediators.R
 
-# Mediator 1 - controls from small case-control (as Laura did)
-# Logistic model adjusting for mediator. Need theta1 (exposure coef) and theta2 
-# (mediator coef) from this
-modY <- clogit(update(base, ~. + I(Qe_Alc/12) + M.alc), data = crc1) 
+# M1: controls from small case-control, all biocrates compounds (as Laura did)
+# modY is a CLR adjusting for the mediator. Need exposure and mediator coefs theta1, theta2 
+modY <- clogit(update(base, ~. + I(Qe_Alc/12) + M1), data = crc1) 
 
-# Linear model of outcome and mediator (OR from beta coefficients). Need beta1 (exposure coef)
-modM <- lm(M.alc ~ Bmi_C + Qe_Energy + L_School + Smoke_Stat + Smoke_Int + Height_C + 
+# modM is a linear model of outcome and mediator. Need exposure coef beta1
+modM <- lm(M1 ~ Bmi_C + Qe_Energy + L_School + Smoke_Stat + Smoke_Int + Height_C + 
               Qge0701 + I(Qe_Alc/12), data = crc1)
 
-# Natural direct effect is given as exp(coeff for 1 unit change in exposure) theta1
-theta1s <- tidy(modY, exponentiate = T, conf.int = T)[20, -1] 
+# Natural direct effect is given as exp(coeff for 1 unit change in exposure), theta1
+theta1 <- tidy(modY, exponentiate = T, conf.int = T) %>% filter(term == "I(Qe_Alc/12)") %>% select(-1)
 
-#NDE = 1.08 (0.95-1.22)
-#      1.07 (0.94-1.22) for new mediator derived without zero alc values
+# Natural indirect effect is exp(coeff of theta for mediator x beta for exposure)
+# Get Theta coefficients of mod Y and beta coefficients of modM:
+theta2 <- tidy(modY, conf.int = T) %>% filter(term == "M1") %>% select(-1)
+beta1  <- tidy(modM, conf.int = T) %>% filter(term == "I(Qe_Alc/12)") %>% select(-1)
+NIE <- exp(theta2*beta1)
 
-# Natural indirect effect is given as exp(coeff of theta for mediator x beta for exposure)
-theta2s <- tidy(modY, conf.int = T)[21, -1]
-theta2s <- tidy(modY, conf.int = T)[21, ]
-
-# For betas need to get CI separately
-beta1s <- tidy(modM, conf.int = T)[21, -1] %>% as.numeric
-ci     <- confint(modM, "I(Qe_Alc/12)")
-beta1s <- c(beta1s, ci)
-exp(theta2s*beta1s)
-# NIE = 1.08 (1.02-1.17)
-#       1.08 (1.02-1.18) for new mediator derived without zero alc values
-
-# Summary
-# TE = 1.14 (1.00-1.28)
-# NDE = 1.07 (0.94-1.22)
-# NIE = 1.08 (1.02-1.18)
+# TE = 1.14 (1.00-1.28), NDE = 1.07 (0.94-1.22), NIE = 1.10 (1.02-1.23)
 
 # Risk difference ratio is defined as log(TE) - log(NDE) / log (NDE)  *100
 logTE <- coef(fit3)[20] 
 logNDE <- coef(modY)[20]
-RDR <- 100 * (logTE - logNDE) / logNDE # 74.0%; 86.9% with new mediator
+RDR <- 100 * (logTE - logNDE) / logNDE # 90.6
 
-
-### Mediator 1 with gate variable
+# With gate variable
 # Natural direct effect is given as exp(coeff for 1 unit change in exposure) theta1
-modY <- clogit(update(base, ~. + I(Qe_Alc/12) + Qe_Alc_cat + M.alc), data = crc1) 
-theta1s <- tidy(modY, exponentiate = T, conf.int = T)[20, -1] #NDE = 1.11 (0.97-1.28)
-theta2s <- tidy(modY, conf.int = T)[21, -1]
+modY <- clogit(update(base, ~. + I(Qe_Alc/12) + Qe_Alc_cat + M1), data = crc1) 
+theta1 <- tidy(modY, exponentiate = T, conf.int = T) %>% filter(term == "I(Qe_Alc/12)") %>% select(-1)
+theta2 <- tidy(modY, conf.int = T) %>% filter(term == "M1") %>% select(-1)
 
 # Linear model of outcome and mediator (OR from beta coefficients). Need beta1 (exposure coef)
-modM <- lm(M.alc ~ Bmi_C + Qe_Energy + L_School + Smoke_Stat + Smoke_Int + Height_C + 
+modM <- lm(M1 ~ Bmi_C + Qe_Energy + L_School + Smoke_Stat + Smoke_Int + Height_C + 
              Qge0701 + I(Qe_Alc/12) + Qe_Alc_cat, data = crc1)
-# Need to get CIs separately
-beta1s <- tidy(modM, conf.int = T)[21, -1] %>% as.numeric
-ci     <- confint(modM, "I(Qe_Alc/12)")
-beta1s <- c(beta1s, ci)
-exp(theta2s*beta1s)
-# NIE = 0.98 (0.96-1.01)
 
-# Summary
-# TE = 1.18 (1.03-1.34)
-# NDE = 1.11 (0.97-1.28)
+beta1 <- tidy(modM, conf.int = T) %>% filter(term == "I(Qe_Alc/12)") %>% select(-1)
+NIE <- exp(theta2*beta1)
+
+# TE = 1.18 (1.03-1.34), NDE = 1.11 (0.96-1.27), NIE = 1.10 (1.01-1.24)
 
 logTE <- coef(fit4)[20] 
 logNDE <- coef(modY)[20]
-RDR <- 100 * (logTE - logNDE) / logNDE # 51.2%
+RDR <- 100 * (logTE - logNDE) / logNDE # 61.6%
 
 
-### Mediator 2 - EPIC pooled controls
-modY <- clogit(update(base, ~. + I(Qe_Alc/12) + M.alc1), data = crc1) 
-
-# Linear model of outcome and mediator (OR from beta coefficients). Need beta1 (exposure coef)
-modM <- lm(M.alc1 ~ Bmi_C + Qe_Energy + L_School + Smoke_Stat + Smoke_Int + Height_C + 
+# M2: controls from small study, amino acids only
+modY <- clogit(update(base, ~. + I(Qe_Alc/12) + M2), data = crc1) 
+modM <- lm(M2 ~ Bmi_C + Qe_Energy + L_School + Smoke_Stat + Smoke_Int + Height_C + 
              Qge0701 + I(Qe_Alc/12), data = crc1)
 
 # Natural direct effect is given as exp(coeff for 1 unit change in exposure) theta1
-theta1s <- tidy(modY, exponentiate = T)[20, -1] #NDE = 1.16 (1.02-1.31)
-
-# Natural indirect effect is given as exp(coeff of theta for mediator x beta for exposure)
-theta2s <- tidy(modY, conf.int = T)[21, -1]
-
-# For betas need to get CI separately
-beta1s <- tidy(modM)[21, -1] %>% as.numeric
-ci     <- confint(modM, "I(Qe_Alc/12)")
-beta1s <- c(beta1s, ci)
-exp(theta2s*beta1s)
-# NIE = 0.98 (0.97-1.01)
-
-# Summary
-# TE = 1.14 (1.00-1.28)
-# NDE = 1.16 (1.02-1.31)
-
-# Risk difference ratio is defined as log(TE) - log(NDE) / log (NDE)  *100
+theta1 <- tidy(modY, exponentiate = T, conf.int = T) %>% filter(term == "M2") %>% select(-1)
+theta2 <- tidy(modY, conf.int = T) %>% filter(term == "M2") %>% select(-1)
+beta1  <- tidy(modM, conf.int = T) %>% filter(term == "I(Qe_Alc/12)") %>% select(-1)
+NIE <- exp(theta2 * beta1)
+# TE = 1.14 (1.00-1.28), NDE = 1.14 (1.01-1.29), NIE = 0.99 (1.00-1.00)
 logTE <- coef(fit3)[20] 
 logNDE <- coef(modY)[20]
-RDR <- 100 * (logTE - logNDE) / logNDE # -12.9% (-13.4 for new mediator with zeros excluded)
+RDR <- 100 * (logTE - logNDE) / logNDE # -3.9
 
-# With gate variable
-modY <- clogit(update(base, ~. + I(Qe_Alc/12) + Qe_Alc_cat + M.alc1), data = crc1) 
-
-# Linear model of outcome and mediator (OR from beta coefficients). Need beta1 (exposure coef)
-modM <- lm(M.alc1 ~ Bmi_C + Qe_Energy + L_School + Smoke_Stat + Smoke_Int + Height_C + 
+# M2 + gate
+modY <- clogit(update(base, ~. + I(Qe_Alc/12) + Qe_Alc_cat + M2), data = crc1) 
+modM <- lm(M2 ~ Bmi_C + Qe_Energy + L_School + Smoke_Stat + Smoke_Int + Height_C + 
              Qge0701 + I(Qe_Alc/12) + Qe_Alc_cat, data = crc1)
 
 # Natural direct effect is given as exp(coeff for 1 unit change in exposure) theta1
-theta1s <- tidy(modY, conf.int = T)[20, -1] #NDE = 1.16 (1.02-1.31)
-
-# Natural indirect effect is given as exp(coeff of theta for mediator x beta for exposure)
-theta2s <- tidy(modY, conf.int = T)[21, -1]
-
-# For betas need to get CI separately
-beta1s <- tidy(modM)[21, -1] %>% as.numeric
-ci     <- confint(modM, "I(Qe_Alc/12)")
-beta1s <- c(beta1s, ci)
-exp(theta2s*beta1s)
-# NIE = 0.99 (0.98-1.00)
-
-# Summary
-# TE = 1.18 (1.00-1.34)
-# NDE = 1.16 (1.02-1.31)
-# NIE = 0.99 (0.98-1.00)
-
-# Risk difference ratio is defined as log(TE) - log(NDE) / log (NDE)  *100
+theta1 <- tidy(modY, exponentiate = T, conf.int = T) %>% filter(term == "I(Qe_Alc/12)") %>% select(-1)
+theta2 <- tidy(modY, conf.int = T) %>% filter(term == "M2") %>% select(-1)
+beta1  <- tidy(modM, conf.int = T) %>% filter(term == "I(Qe_Alc/12)") %>% select(-1)
+NIE <- exp(theta2 * beta1)
+# TE = 1.18 (1.03-1.34), NDE = 1.19 (1.04-1.35), NIE = 0.99 (1.00-1.00)
 logTE <- coef(fit4)[20] 
 logNDE <- coef(modY)[20]
-RDR <- 100 * (logTE - logNDE) / logNDE # -10.8%
+RDR <- 100 * (logTE - logNDE) / logNDE # -3.9
+
+# M3: controls from small study, lipid metabolites only
+modY <- clogit(update(base, ~. + I(Qe_Alc/12) + M3), data = crc1) 
+modM <- lm(M3 ~ Bmi_C + Qe_Energy + L_School + Smoke_Stat + Smoke_Int + Height_C + 
+             Qge0701 + I(Qe_Alc/12), data = crc1)
+
+# Natural direct effect is given as exp(coeff for 1 unit change in exposure) theta1
+theta1s <- tidy(modY, exponentiate = T, conf.int = T) %>% filter(term == "I(Qe_Alc/12)") %>% select(-1)
+theta2s <- tidy(modY, conf.int = T) %>% filter(term == "M3") %>% select(-1)
+beta1s  <- tidy(modM, conf.int = T) %>% filter(term == "I(Qe_Alc/12)") %>% select(-1)
+NIE <- exp(theta2s*beta1s)
+# TE = 1.14 (1.00-1.28), NDE = 1.06 (0.93-1.21), NIE = 1.10 (1.02-1.21)
+logTE <- coef(fit3)[20] 
+logNDE <- coef(modY)[20]
+RDR <- 100 * (logTE - logNDE) / logNDE # 118.6
+
+# M3 + gate
+modY <- clogit(update(base, ~. + I(Qe_Alc/12) + Qe_Alc_cat + M3), data = crc1) 
+modM <- lm(M3 ~ Bmi_C + Qe_Energy + L_School + Smoke_Stat + Smoke_Int + Height_C + 
+             Qge0701 + I(Qe_Alc/12) + Qe_Alc_cat, data = crc1)
+
+# Natural direct effect is given as exp(coeff for 1 unit change in exposure) theta1
+theta1 <- tidy(modY, exponentiate = T, conf.int = T) %>% filter(term == "M3") %>% select(-1)
+theta2 <- tidy(modY, conf.int = T) %>% filter(term == "M3") %>% select(-1)
+beta1  <- tidy(modM, conf.int = T) %>% filter(term == "I(Qe_Alc/12)") %>% select(-1)
+NIE <- exp(theta2*beta1)
+# TE = 1.18 (1.03-1.34), NDE = 1.06 (0.93-1.21), NIE = 1.10 (1.02-1.22)
+logTE <- coef(fit4)[20] 
+logNDE <- coef(modY)[20]
+RDR <- 100 * (logTE - logNDE) / logNDE # 78.0
+
+
+
+### Mediator derived from EPIC non-CRC pooled controls
+modY <- clogit(update(base, ~. + I(Qe_Alc/12) + M4), data = crc1) 
+modM <- lm(M4 ~ Bmi_C + Qe_Energy + L_School + Smoke_Stat + Smoke_Int + Height_C + 
+             Qge0701 + I(Qe_Alc/12), data = crc1)
+
+# Natural direct effect is given as exp(coeff for 1 unit change in exposure) theta1
+theta1 <- tidy(modY, exponentiate = T, conf.int = T) %>% filter(term == "I(Qe_Alc/12)") %>% select(-1)
+theta2 <- tidy(modY, conf.int = T) %>% filter(term == "M4") %>% select(-1)
+beta1 <- tidy(modM, conf.int = T) %>% filter(term == "I(Qe_Alc/12)") %>% select(-1)
+NIE <- exp(theta2*beta1)
+
+#TE = 1.14 (1.00-1.28), NDE = 1.16 (1.02-1.31), NIE = 0.99 (0.98-1.01)
+logTE <- coef(fit3)[20] 
+logNDE <- coef(modY)[20]
+RDR <- 100 * (logTE - logNDE) / logNDE # -11.8%
+
+# With gate variable
+modY <- clogit(update(base, ~. + I(Qe_Alc/12) + Qe_Alc_cat + M4), data = crc1) 
+modM <- lm(M4 ~ Bmi_C + Qe_Energy + L_School + Smoke_Stat + Smoke_Int + Height_C + 
+             Qge0701 + I(Qe_Alc/12) + Qe_Alc_cat, data = crc1)
+
+# Natural direct effect is given as exp(coeff for 1 unit change in exposure) theta1
+theta1 <- tidy(modY, exponentiate = T, conf.int = T) %>% filter(term == "I(Qe_Alc/12)") %>% select(-1)
+theta2 <- tidy(modY, conf.int = T) %>% filter(term == "M4") %>% select(-1)
+beta1 <- tidy(modM, conf.int = T) %>% filter(term == "I(Qe_Alc/12)") %>% select(-1)
+NIE <- exp(theta2 * beta1)
+# TE = 1.18 (1.00-1.34), NDE = 1.20 (1.05-1.37), NIE = 0.99 (0.98-1.01)
+
+logTE <- coef(fit4)[20] 
+logNDE <- coef(modY)[20]
+RDR <- 100 * (logTE - logNDE) / logNDE # -9.5%
+
+### Mediator derived from fatty acids data
+modY <- clogit(update(base, ~. + I(Qe_Alc/12) + M5), data = crc3) 
+modM <- lm(M5 ~ Bmi_C + Qe_Energy + L_School + Smoke_Stat + Smoke_Int + Height_C + 
+             Qge0701 + I(Qe_Alc/12), data = crc3)
+# Natural direct effect is given as exp(coeff for 1 unit change in exposure) theta1
+theta1 <- tidy(modY, exponentiate = T, conf.int = T) %>% filter(term == "M5") %>% select(-1)
+theta2 <- tidy(modY, conf.int = T) %>% filter(term == "M5") %>% select(-1)
+beta1 <- tidy(modM, conf.int = T) %>% filter(term == "I(Qe_Alc/12)") %>% select(-1)
+NIE <- exp(theta2 * beta1)
+# TE = 1.18 (1.00-1.34), NDE = 1.21 (1.06-1.38), NIE = 0.95 (0.93-1.00)
+logTE <- coef(fit5)[19] %>% as.numeric
+logNDE <- coef(modY)[19] %>% as.numeric
+RDR <- 100 * (logTE - logNDE) / logNDE # -29.1%
+
+# Gate variable
+modY <- clogit(update(base, ~. + I(Qe_Alc/12) + Qe_Alc_cat + M5), data = crc3) 
+modM <- lm(M5 ~ Bmi_C + Qe_Energy + L_School + Smoke_Stat + Smoke_Int + Height_C + 
+             Qge0701 + I(Qe_Alc/12) + Qe_Alc_cat, data = crc3)
+# Natural direct effect is given as exp(coeff for 1 unit change in exposure) theta1
+theta1 <- tidy(modY, exponentiate = T, conf.int = T) %>% filter(term == "I(Qe_Alc/12)") %>% select(-1)
+theta2 <- tidy(modY, conf.int = T) %>% filter(term == "M5") %>% select(-1)
+beta1 <- tidy(modM, conf.int = T) %>% filter(term == "I(Qe_Alc/12)") %>% select(-1)
+NIE <- exp(theta2 * beta1)
+# TE = 1.18 (1.00-1.34), NDE = 1.21 (1.06-1.38), NIE = 0.95 (0.93-1.00)
+logTE <- coef(fit6)[19] 
+logNDE <- coef(modY)[19]
+RDR <- 100 * (logTE - logNDE) / logNDE # -9.5%
--- Make_alc_mediators.R
+++ Make_alc_mediators.R
@@ -1,28 +1,48 @@
 # Make mediator variables and bind to small case control (CRC1)
 # Load the 3 CRC studies with all variables
 load("alc_mediation.Rdata")
+
+# Get small case-control study
+#meta <- read_dta("clrt_caco.dta") 
+#crc1 <- read_sas("clrt_caco_metabo.sas7bdat") %>% filter(!is.na(Batch_MetBio) & Country != 6)
+#crc1 <- inner_join(crc1, meta, by = "Idepic", suffix = c("_1", "")) 
+
 library(tidyverse)
 
-# First mediator derived from controls of CRC1
+# Mediators M1, M2, M3, derived from controls of the CRC case-control
 
 # Subset case-control metabolite matrix, log, scale
-expr <- "(carn|oacid|genic|roph|ingo|Sugars)[_]"
-mat <- crc1 %>% select(matches(expr), -contains("tdq")) %>% log2 %>% scale
+expr1 <- "(carn|oacid|genic|roph|ingo|Sugars)[_]"
+expr2 <- "oacid[_]"
+expr3 <- "carn|roph|ingo[_]"
 
-# Subset controls 
-ctlmat <- mat[crc1$Cncr_Caco_Clrt == 0 & crc1$Qe_Alc != 0, ]
-dat <- crc1[crc1$Cncr_Caco_Clrt == 0 & crc1$Qe_Alc != 0, ]
+# Get matrices for all biocrates compounds, amino acids and lipids
+mat1 <- crc1 %>% select(matches(expr1), -contains("tdq")) %>% log2 %>% scale
+mat2 <- crc1 %>% select(matches(expr2), -contains("tdq")) %>% log2 %>% scale
+mat3 <- crc1 %>% select(matches(expr3), -contains("tdq")) %>% log2 %>% scale
+
+# Subset controls
+vec <- crc1$Cncr_Caco_Clrt == 0 & crc1$Qe_Alc != 0
+ctlmat1 <- mat1[vec, ]
+ctlmat2 <- mat2[vec, ]
+ctlmat3 <- mat3[vec, ]
+dat <- crc1[vec, ]
 
 # Adjust matrix and bind log transformed alcohol intakes
 library(lme4)
 adj <- function(x) residuals(lm(x ~ Center + Batch_MetBio, data = dat))
-adjmat <- apply(ctlmat, 2, adj)
-plsdat <- bind_cols(alc = log(dat$Qe_Alc/12 + 0.01), adjmat)
+adjmat1 <- apply(ctlmat1, 2, adj)
+adjmat2 <- apply(ctlmat2, 2, adj)
+adjmat3 <- apply(ctlmat3, 2, adj)
+
+plsdat1 <- bind_cols(alc = log(dat$Qe_Alc/12), adjmat1)
+plsdat2 <- bind_cols(alc = log(dat$Qe_Alc/12), adjmat2)
+plsdat3 <- bind_cols(alc = log(dat$Qe_Alc/12), adjmat3)
 
 # Train PLS. Find the number of dimensions with lowest cross validation error
 library(pls)
 set.seed(111)
-mod <- plsr(alc ~ ., ncomp = 10, data = plsdat, validation = "CV")
+mod <- plsr(alc ~ ., ncomp = 10, data = plsdat1, validation = "CV")
 cv <- RMSEP(mod)
 plot(cv, legendpos = "topright")
 
@@ -32,15 +52,46 @@ selectNcomp(mod, method = "onesigma", plot = F)
 selectNcomp(mod, method = "randomization", plot = T)
 
 # Run PLS with 2 components and get coefficients, predict for mat
-mod1 <- plsr(alc ~ ., data = plsdat, ncomp = 2)
+mod1 <- plsr(alc ~ ., data = plsdat1, ncomp = 1)
 coeff <- data.frame(value = round(coef(mod1)[ , 1, 1], 3))
-crc1$M.alc <- exp(predict(mod1, mat)[,,1])
+crc1$M1 <- exp(predict(mod1, mat1)[,,1])
+
+
+# Amino acids only
+mod <- plsr(alc ~ ., ncomp = 10, data = plsdat2, validation = "CV")
+cv <- RMSEP(mod)
+plot(cv, legendpos = "topright")
+
+# Get no. components by lowest RMSEP, min, "one SE" and "permutation" methods
+which.min(cv$val[estimate = "adjCV", , ]) - 1
+selectNcomp(mod, method = "onesigma", plot = F)
+selectNcomp(mod, method = "randomization", plot = T)
+
+# Run PLS with 2 components and get coefficients, predict for mat
+mod1 <- plsr(alc ~ ., data = plsdat2, ncomp = 1)
+coeff <- data.frame(value = round(coef(mod1)[ , 1, 1], 3))
+crc1$M2 <- exp(predict(mod1, mat2)[,,1])
+
+
+# Lipid metabolites only
+mod <- plsr(alc ~ ., ncomp = 10, data = plsdat3, validation = "CV")
+cv <- RMSEP(mod)
+plot(cv, legendpos = "topright")
+
+# Get no. components by lowest RMSEP, min, "one SE" and "permutation" methods
+which.min(cv$val[estimate = "adjCV", , ]) - 1
+selectNcomp(mod, method = "onesigma", plot = F)
+selectNcomp(mod, method = "randomization", plot = T)
+
+# Run PLS with 2 components and get coefficients, predict for mat
+mod1 <- plsr(alc ~ ., data = plsdat3, ncomp = 1)
+coeff <- data.frame(value = round(coef(mod1)[ , 1, 1], 3))
+crc1$M3 <- exp(predict(mod1, mat3)[,,1])
 
 #fit3a <- clogit(update(base, ~. + M.alc), data = crc1) 
 #OR for mediator 2.63 (1.43-4.81)
 
-
-# Second mediator derived from EPIC pooled controls
+# Mediator M4 derived from EPIC pooled controls
 
 # Get case-control for prediction, log2 and scale as for discovery matrix
 expr <- "(carn|oacid|genic|roph|ingo|Sugars)[_]"
@@ -76,7 +127,7 @@ dat1 <- ctrl[ctrl$Qe_Alc != 0, ]
 library(lme4)
 adj   <- function(x) residuals(lm(x ~ Center + batch_no, data = dat1))
 adjmat <- apply(discmat, 2, adj)
-plsdat <- bind_cols(alc = log(dat1$Qe_Alc/12 + 0.01), adjmat)
+plsdat <- bind_cols(alc = log(dat1$Qe_Alc/12), adjmat)
 
 # Train PLS. Find the number of dimensions with lowest cross validation error
 library(pls)
@@ -91,8 +142,45 @@ selectNcomp(mod, method = "onesigma", plot = F)
 selectNcomp(mod, method = "randomization", plot = T)
 
 # Fit PLSR with 4 components and get coefficients
-mod1 <- plsr(alc ~ ., data = plsdat, ncomp = 4)
+mod1 <- plsr(alc ~ ., data = plsdat, ncomp = 3)
 coeff <- data.frame(value = round(coef(mod)[ , 1, 1], 3))
 
 # Get cases for prediction, log2 and scale as for discovery matrix
-crc1$M.alc1 <- exp(predict(mod1, data.frame(mat))[,,1])
+crc1$M4 <- exp(predict(mod1, data.frame(mat))[,,1])
+
+# Fatty acids mediator from controls of CRC1
+crc3 <- crc3.ph %>% ungroup()
+
+# Remove problem FAs and ones with > 20% CV
+concs <- crc3 %>% select(P14_0 : PCLA_9t_11c, -P24_0, -P20_0)
+concs <- concs %>% select(-P14_1n_5, -P18_1n_7t, -P22_0, -P18_2n_6tt)
+
+# Subset controls
+vec <- crc3$Cncr_Caco_Clrt == 0 & crc3$Qe_Alc != 0
+ctlmat <- concs[vec, ]
+dat <- crc3[vec, ]
+
+# Adjust matrix and bind log transformed alcohol intakes
+library(lme4)
+adj <- function(x) residuals(lm(x ~ Center + Batch_Fa, data = dat))
+adjmat <- apply(ctlmat, 2, adj)
+plsdat <- bind_cols(alc = log(dat$Qe_Alc/12), adjmat)
+
+# Train PLS. Find the number of dimensions with lowest cross validation error
+library(pls)
+set.seed(111)
+mod <- plsr(alc ~ ., ncomp = 10, data = plsdat, validation = "CV")
+cv <- RMSEP(mod)
+plot(cv, legendpos = "topright")
+
+# Get components with lowest RMSEP, min, "one SE" and "permutation" methods
+which.min(cv$val[estimate = "adjCV", , ]) - 1
+selectNcomp(mod, method = "onesigma", plot = F)
+selectNcomp(mod, method = "randomization", plot = T)
+
+# Fit PLSR with 4 components and get coefficients
+mod1 <- plsr(alc ~ ., data = plsdat, ncomp = 2)
+coeff <- data.frame(value = round(coef(mod)[ , 1, 1], 3))
+
+# Get cases for prediction, log2 and scale as for discovery matrix
+crc3$M5 <- exp(predict(mod1, data.frame(concs))[,,1])
