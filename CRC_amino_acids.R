aabe76e0c36f4a2172db3a1d55ad9435bf4d7e3c
Joe <joerothwell1@gmail.com>
2021-08-26 23:26:08 +0200

    Merged two amino acid studies

--- CRC_amino_acids.R
+++ CRC_amino_acids.R
@@ -1,36 +1,13 @@
-# CRC amino acids study. Get data from CRC_prep_data and remove unneeded objects
-# Objective is to make four tables of ORs and MAs: continuous, Q2, Q3, Q4 (vs Q1)
-source("CRC_prep_data.R")
-#source("functions_misc.R")
-rm(list = ls(pattern = "crc|dist|prox|rect"))
-
-# Subset whole colon study. Get compounds and filter those with more than 31% NAs
-mat0 <- colon %>% select(contains("Aminoacid_")) %>% select_if(~ sum(is.na(.)) < (nrow(colon)*0.31))
-
-# Subset follow up range if necessary: 2 years
-colon1 <- colon1 %>% filter(Tfollowup >= 2)
-colon2 <- colon2 %>% filter(Tfollowup >= 2)
-# 5 years
-colon1 <- colon1 %>% filter(Tfollowup >= 5)
-colon2 <- colon2 %>% filter(Tfollowup >= 5)
-# 10 years
-colon1 <- colon1 %>% filter(Tfollowup >= 10)
-colon2 <- colon2 %>% filter(Tfollowup >= 10)
-
-# Subset local and metastatic disease. Variable name is Stagclrt
-# Localised, 1 and 2
-colon1 <- colon1 %>% filter(Stagclrt %in% 1:2)
-colon2 <- colon2 %>% filter(Stagclrt %in% 1:2)
-# Metastatic groups 3,4,5
-colon1 <- colon1 %>% filter(Stagclrt %in% 3:5)
-colon2 <- colon2 %>% filter(Stagclrt %in% 3:5)
-
 # Separate studies for Jelena's paper. p180 (small) had 740 subjects, p150 (large) had 556
 # Note: no NAs and no zeros in these data
-mat1 <- colon1 %>% select(colnames(mat0)) #738, 698 w/o Greece
-mat2 <- colon2 %>% select(colnames(mat0)) #564, 556 w/o Greece
-scalemat1 <- scale(mat1)
-scalemat2 <- scale(mat2)
+source("CRC_prep_data.R")
+rm(list = ls(pattern = "crc|dist|prox|rect"))
+
+mat0 <- colon %>% select(contains("Aminoacid_")) %>% 
+  select_if(~ sum(is.na(.)) < (nrow(colon)*0.31))
+
+mat <- colon %>% select(colnames(mat0)) #738, 698 w/o Greece
+scalemat <- scale(mat)
 
 ### Continuous models per SD increase
 # Define function to apply across quartiles (already matched by lab)
@@ -44,35 +21,18 @@ multiclr <- function(AAconc, dat) {
 }
 
 library(broom)
-cont1 <- apply(scalemat1, 2, multiclr, dat = colon1) %>% 
+mods <- apply(scalemat, 2, multiclr, dat = colon) %>% 
   map_df( ~ tidy(., exponentiate = T, conf.int = T)) %>% filter(term == "AAconc") %>% 
-  mutate(p.adj = p.adjust(p.value, method = "fdr"), compound = colnames(mat1), Phase = "Phase I")
+  mutate(p.adj = p.adjust(p.value, method = "fdr"), compound = colnames(mat), Phase = "Phase I")
 
-cont2 <- apply(scalemat2, 2, multiclr, dat = colon2) %>% 
-  map_df( ~ tidy(., exponentiate = T, conf.int = T)) %>% filter(grepl("AAconc", term)) %>% 
-  mutate(p.adj = p.adjust(p.value, method = "fdr"), compound = colnames(mat2), Phase = "Phase II")
+results <- mods %>% select(Phase, compound, estimate, conf.low, conf.high, p.adj) 
 
-results1 <- cont1 %>% select(Phase, compound, estimate, conf.low, conf.high, p.adj) 
-results2 <- cont2 %>% select(Phase, compound, estimate, conf.low, conf.high, p.adj)
-
-# Meta analysis by nesting
-library(metafor)
-cont <- bind_rows(cont1, cont2) %>% group_by(compound) %>% nest() %>% 
-  mutate(mods = lapply(data, function(df) rma(estimate, sei = std.error, data=df, method="REML")))
-
-# Extract data from models and make data frame
-ma.cont <- lapply(cont$mods, "[", c("b", "ci.lb", "ci.ub", "se", "I2", "QEp"))
-MA.cont <- map_df(ma.cont, bind_rows) %>% 
-  select(estimate = "b", conf.low = "ci.lb", conf.high = "ci.ub", I2, phet = "QEp") %>%
-  bind_cols(compound = results1$compound)
-
-# Bind Phase I and phase II together and format (for sensitivity analysis go to followup)
-tabcont <- bind_rows(results1, results2, MA.cont) %>% arrange(compound) %>%
+# Format OR and 95% CI
+pooled <- results %>%
   mutate_at(vars(estimate:conf.high), ~ format(round(., digits = 2), nsmall = 2)) %>%
-  mutate_at(vars(p.adj:phet), ~ round(., digits = 3)) %>% 
+  #mutate_at(vars(p.adj:phet), ~ round(., digits = 3)) %>% 
   mutate(B1 = " (", hyph = "-", B2 = ")") %>%
   unite("CI", estimate, B1, conf.low, hyph, conf.high, B2, sep = "")
-
 
 # Using map() (not used)
 contD <- bind_rows(cont1, cont2) %>% group_by(compound) %>% nest() %>% 
@@ -87,8 +47,9 @@ followup <- MA.cont %>%
   unite("CI", estimate, B1, conf.low, hyph, conf.high, B2, sep = "")
 
 
-### Categorical analysis. Need to alter function to get categories with cutpoints based on controls
-ctrl <- colon1$Cncr_Caco_Clrt == 0
+### Categorical analysis. Need to alter function to get categories with cutpoints based 
+# on controls
+ctrl <- colon$Cncr_Caco_Clrt == 0
 
 # Using control cutpoints only
 #cutct <- function(x) cut(x, breaks = quantile(x[ctrl]), include.lowest = T, labels = 1:4)
@@ -101,14 +62,14 @@ cutct <- function(x, ...) {
 }
 
 # Apply across continuous matrix to get categories as factor or integer (for p-trend)
-mat3 <- apply(mat1, 2, cutct, labels = 1:4)
-mat3a <- apply(mat1, 2, cutct, labels = F)
+mat1 <- apply(mat, 2, cutct, labels = 1:4)
+mat1a <- apply(mat, 2, cutct, labels = F)
 
 library(broom)
-fits1 <- apply(mat3, 2, multiclr, dat = colon1) %>% 
+fits1 <- apply(mat1, 2, multiclr, dat = colon) %>% 
   map_df( ~tidy(., exponentiate = T, conf.int = T)) %>%
   filter(grepl("AAconc", term)) %>%
-  mutate(Phase = "Phase I", compound = rep(colnames(mat1), each = 3)) %>%
+  mutate(Phase = "Phase I", compound = rep(colnames(mat), each = 3)) %>%
   mutate(p.adj = p.adjust(p.value, method = "fdr"))
 
 # Get p-trend by entering quartile numbers into the model as continuous variables (1,2,3,4)
